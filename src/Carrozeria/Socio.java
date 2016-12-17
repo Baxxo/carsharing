4ae6d82a1ad486edf9c34d177dd7f69415c6ec0c
bassomatteo <bassomatteo@Lab06_06.itseinaudi.local>
2016-12-17 11:00:15 +0100

    lista

---
+++ src/Carrozeria/Socio.java
@@ -0,0 +1,63 @@
+package Carrozeria;
+
+public class Socio {
+
+	String cf;
+	String cognome;
+	String nome;
+	String indirizzo;
+	String telefono;
+	
+	public Socio (String cf, String cognome, String nome, String indirizzo, String telefono){
+		this.cf=cf;
+		this.cognome = cognome;
+		this.nome=nome;
+		this.indirizzo=indirizzo;
+		this.telefono=telefono;
+	}
+	
+	public Socio(){
+		
+	}
+
+	public String getCf() {
+		return cf;
+	}
+
+	public void setCf(String cf) {
+		this.cf = cf;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognonome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+}
