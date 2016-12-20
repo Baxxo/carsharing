2390c922154c934ca3912f19397c88027e29861f
bordinbiadenegianpao <bordinbiadenegianpao@Lab06_07.itseinaudi.local>
2016-12-20 10:57:30 +0100

    insert

--- src/Carrozeria/ConnectionMySql.java
+++ src/Carrozeria/ConnectionMySql.java
@@ -1,11 +1,15 @@
 package Carrozeria;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+
+import com.ibm.icu.util.Calendar;
 
 public class ConnectionMySql {
 	static Connection cn;
@@ -18,7 +22,7 @@ public class ConnectionMySql {
 	int indexSocio;
 	int is = 0;
 	int ja = 0;
-	int kn = 0;
+	static int kn = 0;
 	public ArrayList<Socio> s = new ArrayList<Socio>();
 	public ArrayList<Auto> a = new ArrayList<Auto>();
 	public ArrayList<Noleggio> n = new ArrayList<Noleggio>();
@@ -100,4 +104,44 @@ public class ConnectionMySql {
 		}
 
 	}// fine main
+	public static void nuovoNoleggio(Auto a, Socio s, Date ini, Date fin) throws IOException{
+		Connection cn;
+		Statement st;
+		String sql;
+		// ________________________________connessione
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException: ");
+			System.err.println(e.getMessage());
+		} // fine try-catch
+
+		try {
+			// Creo la connessione al database
+			cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?user=root&password=");
+			
+			Calendar datai = Calendar.getInstance();
+			datai.setTime(ini);
+			Calendar dataf = Calendar.getInstance();
+			dataf.setTime(fin);
+			
+			String sqlDataini = datai.get(Calendar.YEAR) + "-" + datai.get(Calendar.MONTH) + "-"
+					+ datai.get(Calendar.DAY_OF_MONTH); //2016-12-20
+			
+			String sqlDatafin = dataf.get(Calendar.YEAR) + "-" + dataf.get(Calendar.MONTH) + "-"
+					+ dataf.get(Calendar.DAY_OF_MONTH); 
+			sql = "insert into Noleggi (auto,socio,inizio,fine,auto_restituita) values ('" + a.getTarga() + "','" + s.getCf() + "','" +sqlDataini 
+					+ ",'"+ dataf+"','0')";
+			System.out.println(sql);
+			// ________________________________query
+
+			st = cn.createStatement(); // creo sempre uno statement sulla
+										// connessione
+			st.execute(sql); // faccio la query su uno statement
+			cn.close(); // chiusura connessione
+		} catch (SQLException e) {
+			System.out.println("errore:" + e.getMessage());
+		} // fine try-catch
+
+	}
 }// fine classe
